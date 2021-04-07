75dd707512af30a22ca88a1bb54d48c8fa9032f1
DESKTOP-91RVJ1G\asus <hernandomogollon24@gmail.com>
2021-04-07 12:47:43 -0500

    Se agregan cambios para publicar alertas en nuestro servidor y se agregan librerias para un posible envio de datos al micro.

--- app/analitica_modulo.py
+++ app/analitica_modulo.py
@@ -5,6 +5,9 @@ import os
 from sklearn.linear_model import LinearRegression
 import time
 import pika
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
 
 class analitica():
     ventana = 10
@@ -28,6 +31,12 @@ class analitica():
         date_time = now.strftime('%d.%m.%Y %H:%M:%S')
         new_data = {"fecha": date_time, "sensor": msj_vetor[0], "valor": float(msj_vetor[1])}
         self.df = self.df.append(new_data, ignore_index=True)
+        new_data = {"fecha": date_time, "sensor": msj_vetor[2], "valor": float(msj_vetor[3])}
+        self.df = self.df.append(new_data, ignore_index=True)
+
+        self.publicar("volumen1",msj_vetor[1])
+        self.publicar("volumen2",msj_vetor[3])
+
         self.analitica_descriptiva()
         self.analitica_predictiva()
         self.guardar()
@@ -36,8 +45,10 @@ class analitica():
         print(self.df)
 
     def analitica_descriptiva(self):
-        self.operaciones("volumen")
-      
+        self.operaciones("volumen1")
+        self.operaciones("volumen2")
+
+
     def operaciones(self, sensor):
         df_filtrado = self.df[self.df["sensor"] == sensor]
         df_filtrado = df_filtrado["valor"]
@@ -48,8 +59,27 @@ class analitica():
         self.publicar("median-{}".format(sensor), str(df_filtrado.median(skipna = True)))
         self.publicar("std-{}".format(sensor), str(df_filtrado.std(skipna = True)))
 
+        if ("max-{}".format(sensor)=="max-volumen1".format(sensor)) and str(df_filtrado.max(skipna = True))>"70":
+            #publish.single('2/alarmav1', "Alberca llena", hostname='52.54.240.23', client_id='alertamicro')
+            self.publicar("nivelaltov1".format(sensor), "Alberca llena")
+
+        if ("min-{}".format(sensor)=="min-volumen1".format(sensor)) and str(df_filtrado.min(skipna = True))<"60":
+            #publish.single('2/alarmav1', "Alberca vacia", hostname='52.54.240.23', client_id='alertamicro')
+            self.publicar("nivelbajov1".format(sensor), "Alberca vacia")
+
+        if ("max-{}".format(sensor)=="max-volumen2".format(sensor)) and str(df_filtrado.max(skipna = True))>"30":
+            #publish.single('2/alarmav2', "Tanque elevado lleno", hostname='52.54.240.23', client_id='alertamicro')
+            self.publicar("nivelaltov2".format(sensor), "Tanque elevado lleno")
+
+        if ("min-{}".format(sensor)=="min-volumen2".format(sensor)) and str(df_filtrado.min(skipna = True))<"23":
+            #publish.single('2/alarmav2', "Tanque elevado->nivel muy bajo", hostname='52.54.240.23', client_id='alertamicro')
+            self.publicar("nivelaltov2".format(sensor), "Tanque elevado->nivel muy bajo")
+
+
     def analitica_predictiva(self):
-        self.regresion("volumen")
+        self.regresion("volumen1")
+        self.regresion("volumen2")
+
 
     def regresion(self, sensor):
         df_filtrado = self.df[self.df["sensor"] == sensor]
@@ -65,8 +95,8 @@ class analitica():
         range(ultimo_tiempo + tiempo,(self.pronostico + 1) * tiempo + ultimo_tiempo, tiempo)
         nuevos_tiempos = np.array(range(ultimo_tiempo + tiempo,(self.pronostico + 1) * tiempo + ultimo_tiempo, tiempo))
 
-        X = df_filtrado["segundos"].to_numpy().reshape(-1, 1)  
-        Y = df_filtrado["valor"].to_numpy().reshape(-1, 1)  
+        X = df_filtrado["segundos"].to_numpy().reshape(-1, 1)
+        Y = df_filtrado["valor"].to_numpy().reshape(-1, 1)
         linear_regressor = LinearRegression()
         linear_regressor.fit(X, Y)
         Y_pred = linear_regressor.predict(nuevos_tiempos.reshape(-1, 1))
@@ -74,6 +104,7 @@ class analitica():
             time_format = datetime.utcfromtimestamp(tiempo)
             date_time = time_format.strftime('%d.%m.%Y %H:%M:%S')
             self.publicar("prediccion-{}".format(sensor), "{},{}".format(date_time,prediccion[0]))
+            self.publicar("datopredicgrafica-{}".format(sensor), "{:.2f}".format(prediccion[0]))
     @staticmethod
     def publicar(cola, mensaje):
         connexion = pika.BlockingConnection(pika.ConnectionParameters(host='rabbit'))
